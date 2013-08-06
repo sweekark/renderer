82cf2b46f384bd29dd7725ea0cf996c85037c242
sweekark <sweekar07@yahoo.com>
2013-08-06 22:18:24 +0530

    commiting with db server config changes

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,4 @@
-MongoMapper.connection = Mongo::Connection.new('54-251-186-105', 27017,:logger => Rails.logger)
+MongoMapper.connection = Mongo::Connection.new('54.251.169.128', 27017,:logger => Rails.logger)
 MongoMapper.database = "traffic"
 
 if defined?(PhusionPassenger)
